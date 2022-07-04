f6acaf7c7d3222c4308e6b5c77d882be70492b3b
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-07-04 20:47:09 -0300

    Add start of xmenu support
    This support is aimed only at $(dmenu) and $(rofi)
    with the $(-dmenu) flag.

--- src/context.bash
+++ src/context.bash
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+#
+# Shellcheck Global Directives
+#
+#
+# Suppress warnings about using a variable as printf's format string.
+# shellcheck disable=SC2059
+#
+# Suppress warnings about globing and word splitting in unquoted variables.
+# shellcheck disable=SC2086
+#
+
 CONTEXTS_FILE="${PASSWORD_CONTEXTS_FILE:-"${XDG_CONFIG_HOME}/pass/extensions/contexts.cfg"}"
+XMENU="${PASSWORD_CONTEXTS_XMENU:-dmenu}"
+XMENU_FLAGS="${PASSWORD_CONTEXTS_XMENU_FLAGS}"
 
 #
 # Prints an error message to stderr.
 # Globals:
 #   PROGRAM
 #   COMMAND
+#   XMENU
 # Arguments:
 #   1: format string, 2-n: variables for the format string.
 # Outputs:
@@ -16,11 +30,12 @@ CONTEXTS_FILE="${PASSWORD_CONTEXTS_FILE:-"${XDG_CONFIG_HOME}/pass/extensions/con
 rperr() {
   local fmtstr="${1}"
   shift
-  #
-  # The format string as a variable is desired behavior.
-  # shellcheck disable=SC2059
-  #
-  printf "%s: ${fmtstr}" "${PROGRAM} ${COMMAND}" "${@}" 1>&2
+  if tty 1>/dev/null 2>&1; then
+    printf "%s: ${fmtstr}" "${PROGRAM} ${COMMAND}" "${@}" 1>&2
+  else
+    printf "Exit 1" \
+      | "${XMENU}" ${XMENU_FLAGS} -p "$(printf "${fmtstr}" "${@}")"
+  fi
 }
 
 main() {
