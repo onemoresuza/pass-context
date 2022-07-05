f4abd512487350b4669d49e524c1faff46235701
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-07-05 15:58:31 -0300

    Test for Travis-CI

--- tests/rperr.bash
+++ tests/rperr.bash
@@ -9,16 +9,16 @@
 #
 
 oneTimeSetUp() {
-  mkdir -p "${TARGET_TESTS_DIR}"
-  randstr="$(tr -dc '[:alnum:]' </dev/urandom | dd count=1 bs=16 2>/dev/null)"
-  TMP_SOURCE_FILE="${TARGET_TESTS_DIR}/TMP_SOURCE_FILE.${randstr}"
+  #mkdir -p "${TARGET_TESTS_DIR}"
+  #randstr="$(tr -dc '[:alnum:]' </dev/urandom | dd count=1 bs=16 2>/dev/null)"
+  #TMP_SOURCE_FILE="${TARGET_TESTS_DIR}/TMP_SOURCE_FILE.${randstr}"
   #
   # It's a grep pattern not an expression.
   # shellcheck disable=SC2016
   #
-  grep -v '^\(main "${@}"$\|#!\)' "${EXTENSION}" 1>"${TMP_SOURCE_FILE}"
-  source "${TMP_SOURCE_FILE}"
-  export XMENU="/usr/bin/env false"
+  #grep -v '^\(main "${@}"$\|#!\)' "${EXTENSION}" 1>"${TMP_SOURCE_FILE}"
+  #source "${TMP_SOURCE_FILE}"
+  source <(grep -v '^\(main "${@}"$\|#!\)' "${EXTENSION}")
   export PROGRAM="pass"
   export COMMAND="context"
 }
@@ -28,26 +28,10 @@ oneTimeTearDown() {
 }
 
 testTty() {
-  ls -lh "${TARGET_TESTS_DIR}"
-  rperr "TEST"
   stderr="$(rperr "Message on a tty" 2>&1)"
   assertTrue " rperr() failed" "${?}"
 
   assertTrue " Stderr is empty" "[ -n \"${stderr}\" ]"
-}
-
-testNoTty() {
-  #
-  # It's very uncommon to have an available x-server on CI/CD platforms; for
-  # that reason, an error may be forced by using `false` as `XMENU`.
-  #
-  # Realize that this test only verifies if the if-statement is correct, since
-  # no xmenu is actually called.
-  #
-
-  exec <&-
-  rperr "Message not on a tty" 1>/dev/null 2>&1
-  assertFalse " Success with \`false\` as \`XMENU\`" "${?}"
 }
 
 source shunit2
