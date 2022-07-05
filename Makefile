953f5a17f117e68e247ef7b131c189a595daa370
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-07-05 15:10:32 -0300

    Add tests for get_context function

--- Makefile
+++ Makefile
@@ -8,10 +8,14 @@ N := \033[m
 
 all:
 	@printf "Available Targets:\n\n"
-	@printf "$(B)test$(N): run all the \"test-*\" targets.\n"
-	@printf "$(B)test-rperr$(N): run the tests for the \"rperr()\" function.\n"
+	@printf "$(B)test$(N):\t\t\trun all the \"test-*\" targets.\n"
+	@printf "$(B)test-rperr$(N):\t\trun the tests for the \"rperr()\" function.\n"
+	@printf "$(B)test-get_context$(N):\trun the tests for the \"get_context()\" function.\n"
 
 test: test-rperr
 
 test-rperr:
 	EXTENSION=$(EXTENSION) $(SHELL) $(TESTS_DIR)/rperr.bash
+
+test-get_context:
+	EXTENSION=$(EXTENSION) $(SHELL) $(TESTS_DIR)/get_context.bash
---
+++ tests/get_context.bash
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# file: tests/get_context.bash
+
+#
+# Shellcheck Global Directives
+#
+# Suppress warnings about following file paths.
+# shellcheck source=/dev/null
+#
+
+oneTimeSetUp() {
+  #
+  # It's a grep pattern not an expression.
+  # shellcheck disable=SC2016
+  #
+  source <(grep -v '^\(main "${@}"$\|#!\)' "${EXTENSION}")
+  randstr="$(tr -dc '[:alnum:]' </dev/urandom | dd count=1 bs=16 2>/dev/null)"
+  CONTEXTS_FILE="${TMPDIR:-/dev/shm}/context-file.${randstr}"
+  CONTEXT="CONTEXT1"
+}
+
+oneTimeTearDown() {
+  rm -f "${CONTEXTS_FILE}"
+}
+
+tearDown() {
+  : >"${CONTEXTS_FILE}"
+}
+
+testOneContext() {
+  local content line1 line2
+  line1="var1=val1"
+  line2="var2=val2"
+  printf -v content "[%s]\n%s\n%s\n" "${CONTEXT}" "${line1}" "${line2}"
+  printf "%s" "${content}" 1>"${CONTEXTS_FILE}"
+
+  assertEquals " Failed to get correct variables:" \
+    "${line1}"$'\n'"${line2}" "$(get_context "${CONTEXT}")"
+
+  return 0
+}
+
+testMultContexts() {
+  local content line1 line2
+  line1="var1=val1"
+  line2="var2=val2"
+  printf -v content "[%s]\n%s\n%s\n" "${CONTEXT}" "${line1}" "${line2}"
+  printf "%s" "${content}" 1>"${CONTEXTS_FILE}"
+
+  local content2 line1_1 line2_1
+  line1_1="var1=val1"
+  line2_1="var2=val2"
+  printf -v content2 "[%s]\n%s\n%s\n" "${CONTEXT}" "${line1_1}" "${line2_1}"
+  printf "%s" "${content2}" 1>>"${CONTEXTS_FILE}"
+
+  assertEquals " Failed to get correct variables:" \
+    "${line1}"$'\n'"${line2}" "$(get_context "${CONTEXT}")"
+
+  return 0
+}
+
+testEmptyVariables() {
+  get_context "${CONTEXTS_FILE}" 1>/dev/null 2>&1
+  assertFalse " Success with the variable \"variables\" empty" "${?}"
+
+  return 0
+}
+
+source shunit2
