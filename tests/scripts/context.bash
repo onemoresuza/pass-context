ddda343aff90cacb5439188832e614dfc8d87c0a
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-07-07 16:24:34 -0300

    Test for Travis-CI

--- tests/scripts/context.bash
+++ tests/scripts/context.bash
@@ -8,41 +8,40 @@
 # shellcheck source=/dev/null
 #
 
-oneTimeSetUp() {
+unset PASSWORD_STORE_DIR PASSWORD_STORE_KEY PASSWORD_STORE_GPG_OPTS \
+  PASSWORD_STORE_X_SELECTION PASSWORD_STORE_UMASK \
+  PASSWORD_STORE_GENERATED_LENGTH PASSWORD_STORE_CHARACTER_SET \
+  PASSWORD_STORE_CHARACTER_SET_NO_SYMBOLS PASSWORD_STORE_ENABLE_EXTENSIONS \
+  PASSWORD_STORE_EXTENSIONS_DIR PASSWORD_STORE_SIGNING_KEY
 
-  unset PASSWORD_STORE_DIR PASSWORD_STORE_KEY PASSWORD_STORE_GPG_OPTS \
-    PASSWORD_STORE_X_SELECTION PASSWORD_STORE_UMASK \
-    PASSWORD_STORE_GENERATED_LENGTH PASSWORD_STORE_CHARACTER_SET \
-    PASSWORD_STORE_CHARACTER_SET_NO_SYMBOLS PASSWORD_STORE_ENABLE_EXTENSIONS \
-    PASSWORD_STORE_EXTENSIONS_DIR PASSWORD_STORE_SIGNING_KEY
+unset PASSWORD_STORE_CONTEXTS_FILE
 
-  unset PASSWORD_STORE_CONTEXTS_FILE
+export PASSWORD_STORE_ENABLE_EXTENSIONS=true
+export PASSWORD_STORE_EXTENSIONS_DIR="${TARGET_TESTS_DIR}/extensions"
+install -Dm0700 -t "${PASSWORD_STORE_EXTENSIONS_DIR}" "${EXTENSION}"
 
-  export PASSWORD_STORE_ENABLE_EXTENSIONS=true
-  export PASSWORD_STORE_EXTENSIONS_DIR="${TARGET_TESTS_DIR}/extensions"
-  install -Dm0700 -t "${PASSWORD_STORE_EXTENSIONS_DIR}" "${EXTENSION}"
+unset GNUPGHOME
+export GNUPGHOME="${TARGET_TESTS_DIR}/gnupg"
+install -dm0700 "${GNUPGHOME}"
+gpg2 --yes --batch --pinentry-mode loopback --import "${PGP_KEY}"
+trust_gpg_cmd="${GNUPGHOME}/trust_gpg_cmd"
+printf "5\ny\n" 1>"${trust_gpg_cmd}"
+gpg2 --command-file "${trust_gpg_cmd}" --expert --edit-key "${PGP_KEY_ID}" trust
 
-  unset GNUPGHOME
-  export GNUPGHOME="${TARGET_TESTS_DIR}/gnupg"
-  install -dm0700 "${GNUPGHOME}"
-  gpg2 --yes --batch --pinentry-mode loopback --import "${PGP_KEY}"
-  local trust_gpg_cmd="${GNUPGHOME}/trust_gpg_cmd"
-  printf "5\ny\n" 1>"${trust_gpg_cmd}"
-  gpg2 --command-file "${trust_gpg_cmd}" --expert --edit-key "${PGP_KEY_ID}" trust
+PASS_CMD="$(command -v pass)"
+echo $PASS_CMD
 
-  PASS_CMD="$(command -v pass)"
+export PASSWORD_STORE_CONTEXTS_FILE="${TARGET_TESTS_DIR}/contexts.cfg"
+CONTEXT1="CONTEXT1"
+CONTEXT2="CONTEXT2"
+printf "[%s]\nPASSWORD_STORE_DIR=%s\n" \
+  "${CONTEXT1}" "${PWD}/${TARGET_TESTS_DIR}/context1" \
+  1>"${PASSWORD_STORE_CONTEXTS_FILE}"
+printf "[%s]\nPASSWORD_STORE_DIR=%s\n" \
+  "${CONTEXT2}" "${PWD}/${TARGET_TESTS_DIR}/context2" \
+  1>>"${PASSWORD_STORE_CONTEXTS_FILE}"
 
-  export PASSWORD_STORE_CONTEXTS_FILE="${TARGET_TESTS_DIR}/contexts.cfg"
-  CONTEXT1="CONTEXT1"
-  CONTEXT2="CONTEXT2"
-  printf "[%s]\nPASSWORD_STORE_DIR=%s\n" \
-    "${CONTEXT1}" "${PWD}/${TARGET_TESTS_DIR}/context1" \
-    1>"${PASSWORD_STORE_CONTEXTS_FILE}"
-  printf "[%s]\nPASSWORD_STORE_DIR=%s\n" \
-    "${CONTEXT2}" "${PWD}/${TARGET_TESTS_DIR}/context2" \
-    1>>"${PASSWORD_STORE_CONTEXTS_FILE}"
-
-}
+exit 22
 
 oneTimeTearDown() {
   rm -rf "${TARGET_TESTS_DIR:?}/"*
